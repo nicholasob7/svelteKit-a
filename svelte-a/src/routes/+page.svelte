acd9a5f7ed910313cc263dafd3f79cd7f02fd71a
nicholasob7 <nicholasob972@gmail.com>
2023-01-01 21:32:49 +1300

    Open SvelteKit App

---
+++ svelte-a/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">My first SvelteKit App</a>
+		</li>
+		<li>
+			<a href="/posts">Another Page</a>
+		</li>
+	</ul>
+</nav>
+
+<h1>play</h1>
+
+<p>
+	This is the first time I will deliberately 'play' with all the features... Rather than simply
+	follow along with a tutorial.
+</p>
+
+<p>I know Svelte is the right way to go. I need Typescript for a couple of reasons.</p>
+
+<p>
+	My health, wealth and Permaculture. To gain enough expertise that I can monitor nutrient control
+	in a commercially viable ecosystem.
+</p>
+
+<p>
+	To harvest fruit trees in a food forest with swarm drones. I want to do so in high trees and dense
+	foliage.
+</p>
+
+<p>To remove the cheeks of these fruits and plant the seeds in the pith of their forbears.</p>
+
+<p>
+	To remain supremely awake not mundanely transferred. To show visual content of animal families
+	undisturbed by the harvesting process. To others.
+</p>
+
+Yeah to compute the communicative processes of entire ecosystems. To weave the threads of life.
---
+++ svelte-a/src/routes/posts/+page.svelte
@@ -0,0 +1,12 @@
+<nav>
+	<ul>
+		<li>
+			<a href="/">My first SvelteKit App</a>
+		</li>
+		<li>
+			<a href="/posts">Another Page</a>
+		</li>
+	</ul>
+</nav>
+
+<h1>posts</h1>
